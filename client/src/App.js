e57ba62ce6c18bd37efdff9948414c67a59b1ad6
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-23 22:52:04 +0300

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import StockList from "./components/StockList";
+import {Route, Switch} from 'react-router-dom'
+import ViewItem from "./components/ViewItem";
+import NewStock from "./components/NewStock";
 
 function App() {
   const [stock, setStock] = useState([]);
@@ -16,7 +19,20 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <StockList stock={stock}/>
+      <Switch >
+        <Route exact path={"/"} >
+         <StockList stock={stock}/>
+        </Route>
+        <Route exact path={"/stocks"} >
+          <StockList stock={stock} />
+        </Route>
+        <Route axact path={`/stocks/new`} >
+         <NewStock />
+        </Route>
+        <Route axact path={`/stocks/:id`} >
+         <ViewItem/>
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
