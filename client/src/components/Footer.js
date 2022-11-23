85e6f92250a7aa9ee5bb6d6269145568141ff10f
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-23 08:49:21 +0300

    add footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <div className='footer-div'>
+            <div className='footer-item'>This is a Stock Site</div>
+        </div>
+    )
+}
+
+export default Footer;
