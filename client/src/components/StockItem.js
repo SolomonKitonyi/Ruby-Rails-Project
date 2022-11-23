72ad712bed54d9e54a5a7309ed247f7d16082810
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-23 08:48:44 +0300

    add css classes for styling

--- client/src/components/StockItem.js
+++ client/src/components/StockItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function StockItem ({item}) {
+function StockItem ({item,onClick}) {
     return (
         <>
-         <div>
+         <div className='stock-item-div' onClick={onClick}>
             <h2>{item.name}</h2>
             <h3>{item.quantity}</h3>
             <h4>{item.description}</h4>
