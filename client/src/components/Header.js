9b8251cf9c6896ea985ce339c9d3749920e977af
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-23 08:49:36 +0300

    add header

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <div className='header-div'>
+            <div className='header-item'>Stocks</div>
+            <div className='header-item'>Logout</div>
+        </div>
+    )
+}
+
+export default Header;
