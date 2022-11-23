10c32101cb555b56eaf401d41507dd60245eb541
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-23 22:52:41 +0300

    add nav item

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header () {
     return (
         <div className='header-div'>
-            <div className='header-item'>Stocks</div>
+            <Link to={"/stocks"}>
+              <div className='header-item'>Stocks</div>
+            </Link>
+            <Link to={"/stocks/new"}>
+              <div className='header-item'>ADD-STOCK</div>
+            </Link>
             <div className='header-item'>Logout</div>
         </div>
     )
